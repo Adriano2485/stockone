5daa56537e66cca6751c4097e15938c6ed54e3cf
Adriano2485 <adrianosptac@gmail.com>
2025-11-22 16:15:10 -0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,15 +1,9 @@
 // build.gradle.kts (nível do projeto)
-
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        // Define a versão do Android Gradle Plugin (AGP) aqui, evitando conflito
-        classpath("com.android.tools.build:gradle:8.9.1")
-        classpath("com.google.gms:google-services:4.4.3")
-    }
+plugins {
+    id("com.android.application") version "8.9.1" apply false
+    id("com.android.library") version "8.9.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("com.google.gms.google-services") version "4.4.3" apply false
 }
 
 allprojects {
@@ -19,14 +13,13 @@ allprojects {
     }
 }
 
-// Redireciona o build para fora do diretório do projeto
+// Redireciona a pasta build fora do diretório do projeto
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
-    // Garante que os subprojetos (como :app) sejam avaliados corretamente
     project.evaluationDependsOn(":app")
 }
 
