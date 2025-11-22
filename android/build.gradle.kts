ea0f2bd27a0ffaa506b84a273dc799ff060b6ffb
Adriano2485 <adrianosptac@gmail.com>
2025-11-22 09:06:55 -0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,8 +1,8 @@
 // build.gradle.kts (nível do projeto)
 
 plugins {
-    id("com.android.application") version "8.4.0" apply false
-    id("com.android.library") version "8.4.0" apply false
+    id("com.android.application") version "8.9.1" apply false
+    id("com.android.library") version "8.9.1" apply false
     id("com.google.gms.google-services") version "4.4.3" apply false
 }
 
