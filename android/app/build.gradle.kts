3f9d25c3ca7116ad83a97334c73a0129c496d74d
Adriano2485 <adrianosptac@gmail.com>
2026-01-09 05:03:34 -0300

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         applicationId = "com.dusso40.stockone"
         minSdk = 23
         targetSdk = 36
-        versionCode = 13 // se quiser, pode usar flutter.versionCode
+        versionCode = 14 // se quiser, pode usar flutter.versionCode
         versionName = "2.0.3" // se quiser, pode usar flutter.versionName
     }
 
