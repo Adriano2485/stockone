86f904c3f92ef2bceb0ea4dab4b7d2ff5e38954e
Adriano2485 <adrianosptac@gmail.com>
2026-01-09 18:41:52 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6769,7 +6769,7 @@ class _DocumentosState extends State<Documentos> {
   void _shareDocument(String url, String title) async {
     try {
       final tempDir = await getTemporaryDirectory();
-      final filePath = '${tempDir.path}/$title.pdf';
+      final filePath = '${tempDir.path}/$title';
 
       // Baixar o PDF
       final response = await Dio().download(url, filePath);
