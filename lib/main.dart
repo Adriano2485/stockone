03d8169cafcc19958c0e8fa2127f41dd0f2ebd1c
Adriano2485 <adrianosptac@gmail.com>
2026-05-10 19:12:29 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/services.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'dart:convert';
-import 'dart:html' as html;
-import 'package:flutter/foundation.dart' show kIsWeb;
 import 'dart:convert';
 import 'dart:io';
 import 'dart:async';
