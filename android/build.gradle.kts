63067f2200edc0b5f7e6aa3c7a1c2d5de5a06636
Adriano2485 <adrianosptac@gmail.com>
2026-05-17 11:56:21 -0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,19 +1,13 @@
 // build.gradle.kts (nível do projeto)
 
-import org.gradle.api.tasks.Delete
-import org.gradle.api.file.Directory
-
 buildscript {
     repositories {
         google()
         mavenCentral()
     }
-
     dependencies {
-        // Android Gradle Plugin
-        classpath("com.android.tools.build:gradle:8.7.3")
-
-        // Google Services
+        // Define a versão do Android Gradle Plugin (AGP) aqui, evitando conflito
+        classpath("com.android.tools.build:gradle:8.9.1")
         classpath("com.google.gms:google-services:4.4.3")
     }
 }
@@ -26,23 +20,17 @@ allprojects {
 }
 
 // Redireciona o build para fora do diretório do projeto
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory.dir("../../build").get()
-
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
-
-    val newSubprojectBuildDir: Directory =
-        newBuildDir.dir(project.name)
-
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
-
-    // Garante que os subprojetos sejam avaliados corretamente
+    // Garante que os subprojetos (como :app) sejam avaliados corretamente
     project.evaluationDependsOn(":app")
 }
 
-// Limpar build
+// Tarefa para limpar o build
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
